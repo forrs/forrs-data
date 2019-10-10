6b2512a552f948fc1d8e8eac5b617f175551a36f
Follpvosten <wolfi@karpador.xyz>
2019-10-10 12:27:06 +0200

    Id now wraps unsigned values

--- src/model/id.rs
+++ src/model/id.rs
@@ -5,62 +5,32 @@ cfg_if::cfg_if! {
     if #[cfg(any(
         target_pointer_width = "64",
         target_pointer_width = "128",
-        feature = "big-numbers"))
-    ] {
-        pub type Inner = i64;
-        pub type Unsigned = u64;
-        pub type Innerx2 = i128;
+        feature = "big-numbers"
+    ))] {
+        pub type Inner = u64;
+        pub(crate) type Signed = i64;
     } else {
-        pub type Inner = i32;
-        pub type Unsigned = u32;
-        pub type Innerx2 = i64;
+        pub type Inner = u32;
+        pub(crate) type Signed = i32;
     }
 }
 
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq)]
 pub struct Id(pub(crate) Inner);
 
-impl From<Unsigned> for Id {
-    fn from(v: Unsigned) -> Self {
-        let value = if v > Inner::max_value() as Unsigned {
-            // If v is above the max value, we subtract and cast the result.
-            (v - (Inner::max_value() as Unsigned) - 1) as Inner
-        } else {
-            // Otherwise, since v is a valid IdInner, cast it and subtract from that.
-            v as Inner - Inner::max_value() - 1
-        };
-        Self(value)
+impl From<Inner> for Id {
+    fn from(v: Inner) -> Id {
+        Self(v)
+    }
+}
+impl From<Id> for Inner {
+    fn from(id: Id) -> Inner {
+        id.0
     }
 }
 
 impl Display for Id {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let output = self.0 as Innerx2 + Inner::max_value() as Innerx2 + 1;
-        write!(f, "{}", output)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{Id, Inner, Unsigned};
-    #[test]
-    fn correct_display() {
-        assert_eq!(format!("{}", Id(Inner::min_value())), "0".to_string());
-        assert_eq!(
-            format!("{}", Id(0)),
-            format!("{}", Inner::max_value() as Unsigned + 1)
-        );
-        assert_eq!(
-            format!("{}", Id(Inner::max_value())),
-            format!("{}", Unsigned::max_value())
-        );
-    }
-    #[test]
-    fn correct_from() {
-        assert_eq!(Id::from(0 as Unsigned).0, Inner::min_value());
-        assert_eq!(Id::from(Unsigned::max_value()).0, Inner::max_value());
-        assert_eq!(Id::from(Inner::max_value() as Unsigned).0, -1);
-        assert_eq!(Id::from(Inner::max_value() as Unsigned + 1).0, 0);
-        assert_eq!(Id::from(Inner::max_value() as Unsigned + 2).0, 1);
+        write!(f, "{}", Inner::from(*self))
     }
 }
