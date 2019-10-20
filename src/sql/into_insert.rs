c104164313256c7e4370f7438dd164dfa52e11b7
Follpvosten <wolfi@karpador.xyz>
2019-10-20 12:23:40 +0200

    impl IntoInsert for Category

--- src/sql/into_insert.rs
+++ src/sql/into_insert.rs
@@ -1,4 +1,4 @@
-use crate::model::User;
+use crate::model::{User, Category};
 use tokio_postgres::{types::ToSql, Statement, Error, Client};
 
 #[allow(clippy::needless_lifetimes)]
@@ -22,5 +22,14 @@ impl IntoInsert for User {
     }
     fn insert_params<'a>(&'a self) -> Vec<&'a dyn ToSql> {
         vec![&self.name, &self.pw_hash]
+    }
+}
+
+impl IntoInsert for Category {
+    fn insert_stmt() -> &'static str {
+        "INSERT INTO Category (name) VALUES($1)"
+    }
+    fn insert_params<'a>(&'a self) -> Vec<&'a dyn ToSql> {
+        vec![&self.name]
     }
 }
